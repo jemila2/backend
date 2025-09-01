1d7bcec23c0234330a3a50dd5781ca7f02b9c8d3
jemila2 <abubakarjemiletu08@gmail.com>
2025-09-02 00:31:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base: '/backend/',
+   base: './',
   server: {
     open: true,
     port: 3001
